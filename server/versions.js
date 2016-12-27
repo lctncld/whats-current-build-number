ec336bc14eedee5753f625c9c9dc6749c8b14492
Andrey Volodin <lctncld@gmail.com>
2016-12-28 00:38:22 +0300

    Sort by date.

--- server/versions.js
+++ server/versions.js
@@ -9,9 +9,12 @@
     add(config) {
       config = config || {};
       let appVersionStored = this.store.some(e => e.app === config.app);
-      this.store = appVersionStored
+      let store = appVersionStored
         ? this.store.map(e => e.app !== config.app ? e : config)
         : [...this.store, config];
+      this.store = store.sort(function(a, b) {
+        return b.date - a.date;
+      });
     }
   }
   module.exports = Versions;
